5e75ce275906418bc4717bb36ebcb590c7b3690e
Marcio R Nizzola <marcio@nizzola.com.br>
2020-08-18 14:07:28 -0300

    initial projects

---
+++ AppCalculadora/AppCalculadora/AppCalculadora/MainPage.xaml.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCalculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        private double memoriaDeCalculoPreOperacao = 0, memoriaDeCalculoPosOperacao = 0;
+        
+        private string operacao = null, aparecerVisor = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void removerSinaisVisor()
+        {
+            string[] sinais = new string[] { "+", "-", "*", "/" };
+            if (sinais.Contains(aparecerVisor))
+                aparecerVisor = "";
+        }
+
+        private void Btn_igual(object sender, EventArgs e)
+        {
+            try
+            {
+                var memoriaDeCalculoPosOperacao = Convert.ToDouble(visor.Text);
+                double resultado = 0;
+
+                switch (operacao)
+                {
+                    case "+":
+                        resultado = memoriaDeCalculoPreOperacao + memoriaDeCalculoPosOperacao;
+                        break;
+                    case "-":
+                        resultado = memoriaDeCalculoPreOperacao - memoriaDeCalculoPosOperacao;
+                        break;
+                    case "*":
+                        resultado = memoriaDeCalculoPreOperacao * memoriaDeCalculoPosOperacao;
+                        break;
+                    case "/":
+                        resultado = memoriaDeCalculoPreOperacao / memoriaDeCalculoPosOperacao;
+                        break;
+                }
+                visor.Text = resultado.ToString();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void Btn_somar(object sender, EventArgs e)
+        {
+            try
+            {
+                memoriaDeCalculoPreOperacao = Convert.ToDouble(visor.Text);
+                visor.Text = aparecerVisor = operacao = "+";
+            }
+            catch (Exception ex)
+            {
+            visor.Text = ex.Message;
+            }
+            
+        }
+
+        private void Btn_subtrair(object sender, EventArgs e)
+        {
+            try
+            {
+                memoriaDeCalculoPreOperacao = Convert.ToDouble(visor.Text);
+                visor.Text = aparecerVisor = operacao = "-";
+            }
+            catch (Exception ex)
+            {
+                visor.Text = ex.Message;
+            }
+            
+        }
+
+        private void Btn_multiplicar(object sender, EventArgs e)
+        {
+            try
+            {
+                memoriaDeCalculoPreOperacao = Convert.ToDouble(visor.Text);
+                visor.Text = aparecerVisor = operacao = "*";
+            }
+            catch (Exception ex)
+            {
+
+                visor.Text = ex.Message;
+            }
+        }
+
+        private void Btn_dividir(object sender, EventArgs e)
+        {
+            try
+            {
+                memoriaDeCalculoPreOperacao = Convert.ToDouble(visor.Text);
+                visor.Text = aparecerVisor = operacao = "/";
+            }
+            catch (Exception ex)
+            {
+
+                visor.Text = ex.Message;
+            }
+            
+        }
+
+        private void Btn_zerar(object sender, EventArgs e)
+        {
+            try
+            {
+                visor.Text = "0";
+                aparecerVisor = "";
+                memoriaDeCalculoPreOperacao = memoriaDeCalculoPosOperacao = 0;
+                operacao = null;
+            }
+            catch (Exception ex)
+            {
+                visor.Text = ex.Message;
+            }
+            
+        }
+
+        private void Btn_maismenos(object sender, EventArgs e)
+        {
+            try
+            {
+                double valor_visor = Convert.ToDouble(visor.Text);
+                valor_visor = valor_visor * -1;
+                visor.Text = valor_visor.ToString();
+            }
+            catch (Exception ex)
+            {
+                visor.Text = ex.Message;
+            }
+            
+        }
+
+        private void Btn_porcento(object sender, EventArgs e )
+        {
+            try
+            {
+                double valor_visor = Convert.ToDouble(visor.Text);
+                valor_visor = valor_visor / 100;
+                visor.Text = valor_visor.ToString();
+            }
+            catch (Exception ex)
+            {
+                visor.Text = ex.Message;
+            }
+            
+        }
+
+        private void Btn_ponto( object sender, EventArgs e)
+        {
+            aparecerVisor += ponto.Text;
+            visor.Text = aparecerVisor;
+        }
+
+        private void Btn_0( object sender, EventArgs e)
+        {
+            pressNumber(num0.Text);
+        }
+
+        private void Btn_1(object sender, EventArgs e)
+        {
+            pressNumber(num1.Text);
+        }
+
+        private void Btn_2(object sender, EventArgs e)
+        {
+            pressNumber(num2.Text);
+        }
+
+        private void Btn_3(object sender, EventArgs e)
+        {
+            pressNumber(num3.Text);
+        }
+
+        private void Btn_4(object sender, EventArgs e)
+        {
+            pressNumber(num4.Text);
+        }
+
+        private void Btn_5(object sender, EventArgs e)
+        {
+            pressNumber(num5.Text);
+        }
+
+        private void Btn_6(object sender, EventArgs e)
+        {
+            pressNumber(num6.Text);
+        }
+
+        private void Btn_7(object sender, EventArgs e)
+        {
+            pressNumber(num7.Text);
+        }
+
+        private void Btn_8(object sender, EventArgs e)
+        {
+            pressNumber(num8.Text);
+        }
+
+        private void Btn_9(object sender, EventArgs e)
+        {
+            pressNumber(num9.Text);
+        }
+
+        private void pressNumber(string value)
+        {
+            removerSinaisVisor();
+            visor.Text = aparecerVisor += value;
+        }
+
+
+    }
+}
---
+++ app03/App03_IMC/App03_IMC/App03_IMC/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App03_IMC
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double peso = Double.Parse(txt_peso.Text);
+                double altura = Double.Parse(txt_altura.Text);
+
+                double imc = peso / (altura * altura);
+                string classificacao = "";
+
+                if (imc < 17)
+                    classificacao = "Muito abaixo do peso";
+                else if (imc <= 18.49)
+                    classificacao = "Abaixo do peso";
+                else if (imc <= 24.99)
+                    classificacao = "Peso Normal";
+                else if (imc <= 29.99)
+                    classificacao = "Acima do Peso";
+                else if (imc <= 34.99)
+                    classificacao = "Obesidade I";
+                else if (imc <= 39.99)
+                    classificacao = "Obesidade II (severa)";
+                else
+                    classificacao = "Obesidade III (mórbida)";
+
+                resultado.Text = "Seu IMC é " + imc.ToString("0.00") + " está " + classificacao;
+                resultado.TextColor = Color.Red;
+                resultado.HorizontalTextAlignment = TextAlignment.Center;
+            }
+            catch (Exception ex)
+            {
+                resultado.Text = "Desculpe, ocorreu um erro \n " + ex.Message;
+            }
+        }
+    }
+}
